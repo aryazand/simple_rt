4fa69159b45046b508e7b3fd9e05138e17383d39
arya zand <28754724+aryazand@users.noreply.github.com>
2020-12-24 12:39:05 -0600

    add additional source functions

--- _drake.R
+++ _drake.R
@@ -1,8 +1,10 @@
 source("./code/packages.R")
 source("./code/functions.R")
 source("./code/plan.R")
+source("./code/create_figures.R")
+source("./code/COVID19_data_analysis.R")
 
 drake_config(
-  run_simulation,
+  plan,
   verbose = 2
 )
